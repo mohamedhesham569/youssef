108731cd031e164c5541f0e674a33696ec4eebe5
mohamedhesham569 <100250008+mohamedhesham569@users.noreply.github.com>
2024-09-03 02:11:04 +0300

    Update footer.tsx

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -10,8 +10,7 @@ const Footer = () => {
       <div className="flex items-center justify-center gap-1">
         <Typography className="flex items-center" variant="body3">
           <Copyright className="mr-1 inline-block h-4 w-4" />
-          {new Date().getFullYear()} |&nbsp;
-          &nbsp;by Mohamed Hisham
+         by Mohamed Hisham
         </Typography>
       </div>
     </footer>
