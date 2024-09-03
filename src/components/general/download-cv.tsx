3fd6b4029befd781af26dfcfaec54242d5660b91
mohamedhesham569 <100250008+mohamedhesham569@users.noreply.github.com>
2024-09-03 17:48:32 +0300

    Update download-cv.tsx

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -5,7 +5,7 @@ import Button from '@/components/general/button';
 const DownloadCV = () => {
   const handleDownload = () => {
     const link = document.createElement('a');
-    link.href = '/files/Youssef_Fahmy.pdf';
+    link.href = 'https://drive.google.com/file/d/15vo6b5CiOgkBhbqYpp5Em0DZZZg1Kh_o/view?usp=drivesdk';
     link.download = 'Youssef_Fahmy_CV.pdf'; // Name of the file when downloaded
     document.body.appendChild(link);
     link.click();
