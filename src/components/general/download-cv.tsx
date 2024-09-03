6250a8ca9756b2b569fc515a0238113d801a8894
mohamedhesham569 <100250008+mohamedhesham569@users.noreply.github.com>
2024-09-03 17:40:41 +0300

    Update download-cv.tsx

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -3,8 +3,17 @@
 import Button from '@/components/general/button';
 
 const DownloadCV = () => {
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = '/files/Youssef_Fahmy.pdf';
+    link.download = 'Youssef_Fahmy_CV.pdf'; // Name of the file when downloaded
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
-    <Button onClick={() => window?.open('../../../public/files/Youssef_Fahmy.pdf', '_blank')}>
+    <Button onClick={handleDownload}>
       Download CV
     </Button>
   );
