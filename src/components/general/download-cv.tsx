10e0a901869c6fb74b6f22fb90c49b2ecf32f75f
mohamedhesham569 <100250008+mohamedhesham569@users.noreply.github.com>
2024-09-03 17:33:00 +0300

    Update download-cv.tsx

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -4,7 +4,7 @@ import Button from '@/components/general/button';
 
 const DownloadCV = () => {
   return (
-    <Button onClick={() => window?.open('/files/Youssef_Fahmy.pdf', '_blank')}>
+    <Button onClick={() => window?.open('../../../public/files/Youssef_Fahmy.pdf', '_blank')}>
       Download CV
     </Button>
   );
